47784574fbaceec6039e1a50060c50a195f0b297
miljkovicjovan <jovan.miljkovic.biz@gmail.com>
2021-02-08 22:49:57 +0100

    exe

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import os
 from pytube import YouTube 
 
 link = input("Enter link of video: ")
 yt = YouTube(link)
+os.system('cls')
 
 print("Title: ", yt.title)
 print("Views: ", yt.views)
 print("Length: ", yt.length,"seconds")
 print("Ratings: ", yt.rating)
 
-
-ys = yt.streams.get_highest_resolution()
-ys.download()
+while True:
+    choice = input('\nChoose download format (video/audio): ').upper()
+    if choice == "VIDEO":
+        video = yt.streams.get_highest_resolution()
+        video.download()
+        break
+    elif choice == "AUDIO":
+        audio = yt.streams.get_by_itag(140)
+        audio.download()
+        break
+    else:
+        print("Wrong input try again!")
+        print("Enter 'audio' for audio only download")
+        print("Enter 'video' for video download")
